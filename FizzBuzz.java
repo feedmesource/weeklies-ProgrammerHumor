e886ef7cf47f15506a9f34f9d63848385e6439c3
Aimar Tuul <aituul@taltech.ee>
2023-07-07 20:06:33 +0300

    Obscure variable names and remove (almost) all unneeded whitespace :P

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,40 +1,5 @@
 class FizzBuzz {
-    public static void main(String[] args) {
-        for (int num = 1; num <= 100; num++) {
-            StringBuilder line = new StringBuilder("");
-
-            line.append(num % 3 == 0 ? "Fizz" : "");
-            line.append(num % 5 == 0 ? "Buzz" : "");
-            line.append(num % 7 == 0 ? "Rizz" : "");
-            line.append(num % 11 == 0 ? "Jazz" : "");
-            line.append(120 % num == 0 ? "Dizz" : "");
-            line.append(isPrime(num) ? (isPrizz(num) ? "Prizz" : "") : "");
-
-            line.append(line.length() == 0 ? num : "");
-
-            System.out.println(line.toString());
-        }
-    }
-
-    static boolean isPrime (int number) {
-        if (number == 1) {return false;}
-        double sqrt = Math.pow(number, 0.5);
-        for (int i = 2; i <= sqrt ; i++) {
-            if (number % i == 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    static boolean isPrizz(int number) {
-        do {
-            number++;
-            if (number % 7 == 0 || number % 11 == 0) {
-                return true;
-            }
-        } while (!isPrime(number));
-
-        return false;
-    }
+public static void main(String[] args){for(int l3kYz7YGeV=1;l3kYz7YGeV<=100;l3kYz7YGeV++){StringBuilder FOVXrQ1o84=new StringBuilder("");FOVXrQ1o84.append(l3kYz7YGeV%3==0?"Fizz":"");FOVXrQ1o84.append(l3kYz7YGeV%5==0?"Buzz":"");FOVXrQ1o84.append(l3kYz7YGeV%7==0?"Rizz":"");FOVXrQ1o84.append(l3kYz7YGeV%11==0?"Jazz":"");FOVXrQ1o84.append(120%l3kYz7YGeV==0?"Dizz":"");FOVXrQ1o84.append(rsuZbGuOwB(l3kYz7YGeV)?(LQf1N0Orvo(l3kYz7YGeV)?"Prizz":""):"");FOVXrQ1o84.append(FOVXrQ1o84.length()==0?l3kYz7YGeV:"");System.out.println(FOVXrQ1o84.toString());}}
+static boolean rsuZbGuOwB(int jVVjPQMOCy){if(jVVjPQMOCy==1){return false;}double JfPgdwwCS5=Math.pow(jVVjPQMOCy,0.5);for(int i=2;i<=JfPgdwwCS5;i++){if(jVVjPQMOCy%i==0){return false;}}return true;}
+static boolean LQf1N0Orvo(int H2TL8PQZWe){do{H2TL8PQZWe++;if(H2TL8PQZWe%7==0||H2TL8PQZWe%11==0){return true;}}while(!rsuZbGuOwB(H2TL8PQZWe));return false;}
 }
